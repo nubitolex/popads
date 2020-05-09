198bb54697e8d4e59ca342c6e2c30ca19a47a43b
nubitolex <62055485+nubitolex@users.noreply.github.com>
2020-05-09 23:20:10 +0700

    Update popid2.js

--- popid2.js
+++ popid2.js
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
 <script type="text/javascript">
 	$(document).ready(function()
 	{
